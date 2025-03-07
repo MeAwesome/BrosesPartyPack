cd1ced68591deb87abdc28f73e546c498d9180d5
Isaac Robbins <isaacprobbins@gmail.com>
2025-03-07 18:44:30 -0500

    fixed colors

--- app/components/desktop/main-menu/Home.tsx
+++ app/components/desktop/main-menu/Home.tsx
@@ -4,30 +4,29 @@ import { Canvas } from "@react-three/fiber";
 import { CameraControls, Center, RoundedBox, Text3D, useMatcapTexture, useTexture } from "@react-three/drei";
 import { Flex, Box } from "@react-three/flex";
 import GameOption from "~/components/desktop/main-menu/home/GameOption";
+import { NoToneMapping } from "three";
 
 export default function Home() {
 	const { meta } = useLoaderData<typeof loader>();
 
 	return (
 		<div
-			className="bg-cover size-full"
-			style={{
-				backgroundImage: "url('/images/sky.jpg')"
-			}}
+			className="bg-cover size-full bg-[#313338]"
+			// style={{
+			// 	backgroundImage: "url('/images/sky.jpg')"
+			// }}
 		>
-			<Canvas>
+			<Canvas
+				onCreated={({ gl }) => {
+					gl.toneMapping = NoToneMapping;
+				}}
+			>
 				<ambientLight />
 				<Flex justifyContent="center" alignItems="flex-start" position={[-4, 0, 0]} rotation={[0, 0.4, 0]}>
 						{meta.map((game) => (
 							<GameOption key={game.id} gameName={game.name} />
 						))}
-					{/* <Box>
-						<RoundedBox args={[1, 1, 1]} />
-					</Box>
-					<Box>
-						<RoundedBox args={[1, 1, 1]} />
-					</Box> */}
-				</Flex>
+					</Flex>
 				{/* <Text3D font={"/fonts/gt.json"}>Hello</Text3D> */}
 				{/* <GameOption /> */}
 				<CameraControls />
--- app/components/desktop/main-menu/home/GameOption.tsx
+++ app/components/desktop/main-menu/home/GameOption.tsx
@@ -1,37 +1,36 @@
 import { Center, RoundedBox, Text3D, useTexture, MeshDistortMaterial, Sphere, Outlines } from "@react-three/drei";
 import { Box } from "@react-three/flex";
 import { useSpring, animated } from "@react-spring/three";
+import { Box3, Mesh, Vector3 } from "three";
+import { text } from "stream/consumers";
+import { useRef, useLayoutEffect } from "react";
 
 const AnimatedMeshDistortMaterial = animated(MeshDistortMaterial);
 
-export default function GameOptions({
-    gameName
-}: {
-    readonly gameName: string;
-}) {
+export default function GameOptions({ gameName }: { readonly gameName: string }) {
+	const boxRef = useRef<Mesh>(null);
+	const textRef = useRef<Mesh>(null);
+	const size = new Vector3();
 	const purple = useTexture("/textures/68049F_C90DE6_A404CF_B304DC.png");
+
+	useLayoutEffect(() => {
+		if (textRef.current && boxRef.current) {
+			const box = new Box3().setFromObject(textRef.current);
+			const boxSize = box.getSize(size);
+			textRef.current.position.y -= boxSize.y / 2;
+		}
+	}, [textRef, boxRef]);
 
 	return (
 		<Box margin={0.1}>
-			<RoundedBox args={[3, 1, 0.33]}>
-				<meshMatcapMaterial color="white" matcap={purple} />
+			<RoundedBox ref={boxRef} args={[3.5, 1, 0.33]}>
+				<meshBasicMaterial color="#00f" />
+				{/* <meshBasicMaterial color="#2B2D31" /> */}
 			</RoundedBox>
-            {/* <Sphere args={[1, 32, 32]}>
-                <AnimatedMeshDistortMaterial
-                    color={"#0FF"}
-                    distort={0.5}
-                    speed={5}
-                />
-            </Sphere> */}
-			{/* <Center>
-				<Text3D
-                    font={"/fonts/gt.json"}
-                    size={1}
-                >
-					{gameName}
-					<meshMatcapMaterial color="white" matcap={purple} />
-				</Text3D>
-			</Center> */}
+			<Text3D ref={textRef} font={"/fonts/gt.json"} size={1 / 5} position={[-1.7, 0, 0]} rotation={[0, 0, 0]}>
+				#{gameName.replaceAll(" ", "-")}
+				<meshBasicMaterial color="white" />
+			</Text3D>
 		</Box>
 	);
 }
